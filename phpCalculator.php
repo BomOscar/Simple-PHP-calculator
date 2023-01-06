80d71f618cdbbf85ac599b1ba6f1286f805f655c
BomOscar <122103391+BomOscar@users.noreply.github.com>
2023-01-06 17:49:31 +0300

    Update phpCalculator.php

--- phpCalculator.php
+++ phpCalculator.php
@@ -2,7 +2,7 @@
 <!-- code by BomOscar  -->
 <html>
 	<head>
-		<title>Simple Calculator In PHP | BomOscar.com</title>
+		<title>PhpCalculator | BomOscar</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,14 +14,10 @@
 		<div class="container" style="margin-top: 50px">
 		
 			<?php
-			
-				// If the submit button has been pressed
 				if(isset($_POST['submit']))
 				{
-					// Check number values
 					if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
 					{
-						// Calculate total
 						if($_POST['operation'] == 'plus')
 						{
 							$total = $_POST['number1'] + $_POST['number2'];	
@@ -38,28 +34,22 @@
 						{
 							$total = $_POST['number1'] / $_POST['number2'];	
 						}
-						
-						// Print total to the browser
 						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
 					
 					} else {
-						
-						// Print error message to the browser
-						echo 'Numeric values are required';
+						echo 'Only numbers are required';
 					
 					}
 				}
-			// end PHP. Code by BomOscar.com
 			?>
 		    
-		    <!-- Calculator form by BomOscar.com -->
-		    <form method="post" action="calculator.php">
+		    <form method="post" action="phpCalculator.php">
 		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
 		        <select name="operation">
-		        	<option value="plus">Plus</option>
-		            <option value="minus">Minus</option>
+		        	<option value="plus">Add</option>
+		            <option value="minus">Substract</option>
 		            <option value="multiply">Multiply</option>
-		            <option value="divided by">Devide</option>
+		            <option value="divided by">Divide</option>
 		        </select>
 		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
 		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
